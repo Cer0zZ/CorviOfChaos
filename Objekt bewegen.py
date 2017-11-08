8b634965e7485047ca3cfbce35b1ecdee56b6b3c
DerWe <32326219+DerWe@users.noreply.github.com>
2017-11-08 11:00:15 +0100

    Borders mit allem drum und dran, variablen
    Borders mit Variablen changeble

--- Objekt bewegen.py
+++ Objekt bewegen.py
@@ -23,6 +23,15 @@ jump = 0
 jumpdir = 0
 jumpaltitude = 0
 
+topborder = 100
+bottomborder = 700
+leftborder = 100
+rightborder = 900
+
+doordepht = 50
+doortop = 250
+doorbottom = 350
+
 
 while True:
 
@@ -62,34 +71,34 @@ while True:
 #§§§§§§§§§§§§§§§§§§§§§---Check Borders of the Room
 ##################################################################################################        
 #check borders left right   
-    if dir_x > 900 and dir_y < 240 or dir_x > 900 and dir_y > 360:
-        dir_x = 900
+    if dir_x > rightborder and dir_y < (doortop-10) or dir_x > rightborder and dir_y > (doorbottom+10):
+        dir_x = rightborder
         print("x+border passed")
-    if dir_x < 100 and dir_y < 240 or dir_x < 100 and dir_y > 360:
-        dir_x = 100
+    if dir_x < leftborder and dir_y < (doortop-10) or dir_x < leftborder and dir_y > (doorbottom+10):
+        dir_x = leftborder
         print("x-border passed")
         
 #check door borders left right  
-    if dir_x > 950 and dir_y > 250 or dir_x > 950 and dir_y < 350:
-        dir_x = 950
+    if dir_x > (rightborder+doordepht) and dir_y > doortop or dir_x > (rightborder+doordepht) and dir_y < doorbottom:
+        dir_x = (rightborder+doordepht)
         print("x+border passed")
-    if dir_x < 50 and dir_y > 250 or dir_x < 50 and dir_y < 350:
-        dir_x = 50
+    if dir_x < (leftborder-doordepht) and dir_y > doortop or dir_x < (leftborder-doordepht) and dir_y < doorbottom:
+        dir_x = (leftborder-doordepht)
         print("x-border passed")
         
 #check borders top bottom          
-    if dir_y > 600:
-        dir_y = 600
+    if dir_y > bottomborder:
+        dir_y = bottomborder
         print("y+border passed")
-    if dir_y < 100:
-        dir_y = 100
+    if dir_y < topborder:
+        dir_y = topborder
         print("y-border passed")
 #check door borders top bottom  
-    if dir_x < 100 and dir_y > 350 or dir_x > 900 and dir_y > 350:
-        dir_y = 350
+    if dir_x < leftborder and dir_y > doorbottom or dir_x > rightborder and dir_y > doorbottom:
+        dir_y = doorbottom
         print("y+border passed")
-    if dir_x < 100 and dir_y < 250 or dir_x > 900 and dir_y < 250:
-        dir_y = 250
+    if dir_x < leftborder and dir_y < doortop or dir_x > rightborder and dir_y < doortop:
+        dir_y = doortop
         print("y-border passed")
         
 #    print(dir_y,dir_x)
