ad989e726c89ec7df7827e5f950b9e454d069e16
beckerfe <32830189+beckerfe@users.noreply.github.com>
2017-11-08 11:04:25 +0100

    Update Objekt bewegen.py

--- Objekt bewegen.py
+++ Objekt bewegen.py
@@ -1,6 +1,8 @@
 import pygame, time, math
+hight = 1000
+weight = 800
 #screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-screen = pygame.display.set_mode((1000,800))
+screen = pygame.display.set_mode((hight,weight))
 screen_mode = 0 #toggle fullscreen
 clock = pygame.time.Clock()
 FRAMES_PER_SECOND = 30
@@ -64,7 +66,7 @@ while True:
             screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
             screen_mode += 1
         else:
-            screen = pygame.display.set_mode((1000,800))
+            screen = pygame.display.set_mode((hight,weight))
             screen_mode += -1
             
 ##################################################################################################
